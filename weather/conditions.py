c909f238bd5a0418b6741f9412bea8ab49d85b3c
Aykinn <ayckinn@pm.me>
2020-06-26 10:32:46 +0200

    Add cloudy condition

--- weather/conditions.py
+++ weather/conditions.py
@@ -21,29 +21,31 @@ class LSCondition:
 
     def condition_display(self, color):
         self.show_c = self.c_font.render(self.condition_request(), True, color)
-        self.center_x = (256 + (256 / 2) - (self.show_c.get_width() / 2) - 15)
-        self.screen.master.blit(self.show_c, (self.center_x, 520))
+        self.center_x = (256 + (256 / 2)) - (self.show_c.get_width() / 2)
+        self.screen.master.blit(self.show_c, (self.center_x, 530))
 
     def condition_light(self):
         self.condition_display(cst.WHITE)
-        self.condition_pics(cst.SUN, cst.FEW_CLOUDY_SUN, cst.CLOUDY_DAY,
-                            cst.LIGHT_RAIN_DAY, cst.STORM_DAY)
+        self.condition_pics(cst.SUN, cst.FEW_CLOUDY_SUN, cst.CLOUDY_SUN,
+                            cst.CLOUDY_DAY, cst.LIGHT_RAIN_DAY, cst.STORM_DAY)
 
     def condition_dark(self):
         self.condition_display(cst.GREY)
-        self.condition_pics(cst.MOON, cst.FEW_CLOUDY_MOON, cst.CLOUDY_NIGHT,
-                            cst.LIGHT_RAIN_NIGHT, cst.STORM_NIGHT)
+        self.condition_pics(cst.MOON, cst.FEW_CLOUDY_MOON, cst.CLOUDY_MOON,
+                            cst.CLOUDY_NIGHT, cst.LIGHT_RAIN_NIGHT, cst.STORM_NIGHT)
 
-    def condition_pics(self, mode1, mode2, mode3, mode4, mode5):
+    def condition_pics(self, mode1, mode2, mode3, mode4, mode5, mode6):
         if self.condition_request() == "CIEL DÉGAGÉ":
             self.screen.master.blit(mode1, (5, 320))
         if self.condition_request() == "PEU NUAGEUX":
             self.screen.master.blit(mode2, (5, 320))
         if self.condition_request() == "PARTIELLEMENT NUAGEUX":
             self.screen.master.blit(mode2, (5, 320))
-        if self.condition_request() == "NUAGEUX":
+        if self.condition_request() == "COUVERT":
             self.screen.master.blit(mode3, (5, 320))
-        if self.condition_request() == "LÉGÈRE PLUIE":
+        if self.condition_request() == "NUAGEUX":
             self.screen.master.blit(mode4, (5, 320))
-        if self.condition_request() == "ORAGE":
+        if self.condition_request() == "LÉGÈRE PLUIE":
             self.screen.master.blit(mode5, (5, 320))
+        if self.condition_request() == "ORAGE":
+            self.screen.master.blit(mode6, (5, 320))
